d24b84f98a6c565a9df75fdc6f2417916011b934
hamadali06 <ahal06003@utb.vaxjo.se>
2023-09-15 10:07:46 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "vilken är din favorit bok?";
+            Console.WriteLine(x);
+            string bok = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(bok);
+            Console.ReadLine();
+        }
+    }
+}
